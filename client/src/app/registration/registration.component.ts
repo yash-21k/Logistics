f5698b605dd66a2f9f6095b4641ce10b34c04d69
username <wecp-dummy@gmail.com>
2024-02-29 06:59:46 +0000

    Yash3

--- client/src/app/registration/registration.component.ts
+++ client/src/app/registration/registration.component.ts
@@ -2,33 +2,31 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HttpService } from '../../services/http.service';
-
-
+ 
+ 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent {
-
+ 
   itemForm: FormGroup;
   formModel:any={role:null,email:'',password:'',username:''};
   showMessage:boolean=false;
-
+ 
   responseMessage: any;
-  constructor(public router:Router, private bookService:HttpService, private formBuilder: FormBuilder) { 
-    
-      this.itemForm = this.formBuilder.group({
-        email: [this.formModel.email,[ Validators.required, Validators.email]],
-        password: [this.formModel.password,[ Validators.required]],
-        role: [this.formModel.role,[ Validators.required]],
-        username: [this.formModel.username,[ Validators.required]],
-       
-      //complete this function 
-       
-    });
-  }
-
+  constructor(public router:Router, private httpService:HttpService, private formBuilder: FormBuilder) {
+   
+    this.itemForm = this.formBuilder.group({
+      username:[this.formModel.username,[Validators.required,Validators.pattern("^[A-Za-z]\\w{5,19}$")]],
+      email:[this.formModel.email,[Validators.required,Validators.email]],
+      password:[this.formModel.password,[Validators.required,Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}$")]],
+      role:[this.formModel.role,[Validators.required]]
+    //complete this function
+     
+  });
+}
   ngOnInit(): void {
   }
   onRegister()
@@ -36,19 +34,20 @@ export class RegistrationComponent {
     if(this.itemForm.valid)
     {
       this.showMessage=false;
-      this.bookService.registerUser(this.itemForm.value).subscribe(data=>{    
+      this.httpService.registerUser(this.itemForm.value).subscribe(data=>{    
         debugger;
-        this.showMessage=false;
+        this.showMessage=true;
         this.responseMessage='Welcome '+data.name+" you are successfully registered";
         this.itemForm.reset();
-        
+       
       },error=>{ })
     }
     else{
       this.itemForm.markAllAsTouched();
     }
-  //complete this function 
+  //complete this function
   }
-
-
+ 
+ 
 }
+ 
--- server/src/main/java/com/wecp/logisticsmanagementandtrackingsystem/jwt/JwtUtil.java
+++ server/src/main/java/com/wecp/logisticsmanagementandtrackingsystem/jwt/JwtUtil.java
@@ -1,82 +1,82 @@
-// package com.wecp.logisticsmanagementandtrackingsystem.jwt;
-// import com.wecp.logisticsmanagementandtrackingsystem.entity.User;
-// import com.wecp.logisticsmanagementandtrackingsystem.repository.UserRepository;
-// import io.jsonwebtoken.Claims;
-// import io.jsonwebtoken.Jwts;
-// import io.jsonwebtoken.SignatureAlgorithm;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.stereotype.Component;
+package com.wecp.logisticsmanagementandtrackingsystem.jwt;
+import com.wecp.logisticsmanagementandtrackingsystem.entity.User;
+import com.wecp.logisticsmanagementandtrackingsystem.repository.UserRepository;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
-// import java.util.Date;
-// import java.util.HashMap;
-// import java.util.Map;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
-// @Component
-// public class JwtUtil {
+@Component
+public class JwtUtil {
 
-//     private UserRepository userRepository;
+    private UserRepository userRepository;
 
-//     @Autowired
-//     public JwtUtil(UserRepository userRepository) {
-//         this.userRepository = userRepository;
-//     }
+    @Autowired
+    public JwtUtil(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
-//     private final String secret = "secretKey000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+    private final String secret = "secretKey000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
 
-//     private final int expiration = 86400;
+    private final int expiration = 86400;
 
-//     public String generateToken(String username) {
-//         Date now = new Date();
-//         Date expiryDate = new Date(now.getTime() + expiration * 1000);
-//         User user = userRepository.findByUsername(username);
+    public String generateToken(String username) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + expiration * 1000);
+        User user = userRepository.findByUsername(username);
 
-//         Map<String, Object> claims = new HashMap<>();
-//         claims.put("sub", username);
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("sub", username);
 
-//         // Assign role based on user type
-//         claims.put("role", user.getRole());
+        // Assign role based on user type
+        claims.put("role", user.getRole());
 
-//         return Jwts.builder()
-//                 .setClaims(claims)
-//                 .setIssuedAt(now)
-//                 .setExpiration(expiryDate)
-//                 .signWith(SignatureAlgorithm.HS512, secret)
-//                 .compact();
-//     }
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
+    }
 
-//     public Claims extractAllClaims(String token) {
-//         Claims claims;
-//         try {
-//             claims = Jwts.parser()
-//                     .setSigningKey(secret)
-//                     .parseClaimsJws(token)
-//                     .getBody();
-//         } catch (Exception e) {
-//             claims = null;
-//         }
-//         return claims;
-//     }
+    public Claims extractAllClaims(String token) {
+        Claims claims;
+        try {
+            claims = Jwts.parser()
+                    .setSigningKey(secret)
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (Exception e) {
+            claims = null;
+        }
+        return claims;
+    }
 
-//     public String extractUsername(String token) {
-//         Claims claims = Jwts.parser()
-//                 .setSigningKey(secret)
-//                 .parseClaimsJws(token)
-//                 .getBody();
-//         return claims.getSubject();
-//     }
+    public String extractUsername(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(secret)
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
 
-//     public boolean isTokenExpired(String token) {
-//         Date expirationDate = Jwts.parser()
-//                 .setSigningKey(secret)
-//                 .parseClaimsJws(token)
-//                 .getBody()
-//                 .getExpiration();
-//         return expirationDate.before(new Date());
-//     }
+    public boolean isTokenExpired(String token) {
+        Date expirationDate = Jwts.parser()
+                .setSigningKey(secret)
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration();
+        return expirationDate.before(new Date());
+    }
 
-//     public boolean validateToken(String token, UserDetails userDetails) {
-//         final String username = extractUsername(token);
-//         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
-//     }
-// }
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+}
